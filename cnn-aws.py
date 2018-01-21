42f6458d29f82af4d94120dd9e615eec1e579271
Leifu Chen <leifu.chen@gmail.com>
2018-01-21 12:43:36 +0200

    Added dropout layer

--- cnn-aws.py
+++ cnn-aws.py
@@ -65,35 +65,37 @@ from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
 
 batch_size = 64
-epochs = 20
+epochs = 2
 num_classes = 10
 
 fashion_model = Sequential()
-fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(28,28,1),padding='same'))
+fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',padding='same',input_shape=(28,28,1)))
 fashion_model.add(LeakyReLU(alpha=0.1))
 fashion_model.add(MaxPooling2D((2, 2),padding='same'))
+fashion_model.add(Dropout(0.25))
 fashion_model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
 fashion_model.add(LeakyReLU(alpha=0.1))
 fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+fashion_model.add(Dropout(0.25))
 fashion_model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
 fashion_model.add(LeakyReLU(alpha=0.1))                  
 fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+fashion_model.add(Dropout(0.4))
 fashion_model.add(Flatten())
 fashion_model.add(Dense(128, activation='linear'))
-fashion_model.add(LeakyReLU(alpha=0.1))                  
+fashion_model.add(LeakyReLU(alpha=0.1))           
+fashion_model.add(Dropout(0.3))
 fashion_model.add(Dense(num_classes, activation='softmax'))
-
-
-fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
-
 
 fashion_model.summary()
 
+fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
 
 fashion_train = fashion_model.fit(train_X, train_label, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_label))
 
+#fashion_model.save("fashion_model_dropout.h5py")
 
-test_eval = fashion_model.evaluate(test_X, test_Y_one_hot, verbose=0)
+test_eval = fashion_model.evaluate(test_X, test_Y_one_hot, verbose=1)
 print('Test loss:', test_eval[0])
 print('Test accuracy:', test_eval[1])
 
